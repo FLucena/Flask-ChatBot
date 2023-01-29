43003d411bf8a3e6505fc507ba76e48861ae7648
Francisco Lucena <franciscolucena90@gmail.com>
2023-01-28 23:35:39 -0300

    Fix index.py

--- api/index.py
+++ api/index.py
@@ -1,11 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template, request, jsonify
+from chat import get_response
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
 
-@app.route('/about')
-def about():
-    return 'About'
+@app.get("/")
+def index_get():
+    return render_template("base.html")
+
+
+@app.post("/predict")
+def predict():
+    text = request.get_json().get("message")
+    # TODO: check if text is valid
+    response = get_response(text)
+    message = {"answer": response}
+    return jsonify(message)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
